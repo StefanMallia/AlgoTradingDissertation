5a1a98d1892f01c1a3b18a5413c9acf59682e7fc
StefanMallia <stefanmallia@gmail.com>
2021-02-24 23:15:13 +0100

    cleanup

--- 2movaverage unfinished.py
+++
@@ -1,4 +0,0 @@
-datafile=open ('C:/Users/Stefan Mallia/Bachelor of Commerce/Bachelor of Commerce Year 4/Semester 2/Dissertation/Matlab Code/Finished algorithms/Finished algorithms/Movingaverage/2 parameters/EURUSD_hour.csv', 'r')
-data=[]
-for row in datafile:
-    data.append(row.strip().split(','))
